bda15addb9127df2df9299458a69304e131fa38a
hongdonghyun <hong4367@gmail.com>
2018-08-01 19:43:42 +0900

    create query

--- graphql/db.js
+++ graphql/db.js
@@ -1,36 +1,36 @@
 export const people = [
     {
-        id:"0",
+        id:0,
         name : "hong3",
         age : 100,
         company : "herren"
     },
     {
-        id:"1",
+        id:1,
         name : "hongdonghyun",
         age : 90,
         company : "herren"
     },
     {
-        id:"2",
+        id:2,
         name : "2minchul",
         age : 80,
         company : "herren"
     },
     {
-        id:"3",
+        id:3,
         name : "bada",
         age : 70,
         company : "herren"
     },
     {
-        id:"4",
+        id:4,
         name : "daldal",
         age : 60,
         company : "herren"
     },
     {
-        id:"5",
+        id:5,
         name : "nouveau",
         age : 50,
         company : "herren"
@@ -39,6 +39,6 @@ export const people = [
 ]
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => people.id === id)
+    const filteredPeople = people.filter(person => person.id === id)
     return filteredPeople[0]
 }
